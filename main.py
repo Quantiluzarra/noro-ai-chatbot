db9cd6e30daee14a8ba396d333458e0c7b060574
Quantiluzarra <156359929+Quantiluzarra@users.noreply.github.com>
2024-12-02 21:29:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from bot import NoroAIBot
+
+def main():
+    BOT_TOKEN = "8185421413:AAE4hMfApL40d-Dw1WjNadI3ck6kPI1sVcU"
+    bot = NoroAIBot(BOT_TOKEN)
+    bot.start()
+
+if __name__ == "__main__":
+    main()
